7e5a97b305640917c61d2ffee7ee51fd6a5aca03
rahulbahal <rbahal91@gmail.com>
2014-05-07 14:37:47 +0530

    Revert "apache-svn"
    This reverts commit a0850828e38e2159d121ea2eb7308e188c417ad0.

--- src/test/java/myngconnect_server_status/staging/Test_Health_Check_STG.java
+++ src/test/java/myngconnect_server_status/staging/Test_Health_Check_STG.java
@@ -17,15 +17,8 @@ import java.util.Date;
 
 class global
 {
-	public static String content_repository_check = "false";
-	public static String sri1_check = "false";
-	public static String sri2_check = "false";
-	public static String sri3_check = "false";
-	public static String webassets_check = "false";
-	public static String wowza_check = "false";
-	public static String auth_api_check = "false";
-	public static String database_check = "false";
-	
+	public static String global_check = "false";
+	public static String svn="false";
 	String resolution = SetupDriver.resolutionCategory;
 }
 
@@ -56,6 +49,7 @@ class global
 //Take Base Screenshot
 @Ignore 
 public void Test_Health_0_BaseScreenShot()throws Exception{
+System.out.println("In @Ignore");
 Health_check h = new Health_check();
 CSVHandler login_details = new CSVHandler("src/test/resources/login_health_check_staging.csv");
 String baseurl0_name = login_details.getElementXpath("baseurl0_name");
@@ -63,20 +57,22 @@ h.health_login(driver);
 WebElement ele = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/table/tbody/tr[2]/td[2]/div"));
 //Make server CSV driven
 Screenshot.takeElementScreenshot(driver, ele, ele.getLocation(), "base_health_"+baseurl0_name);
+System.out.println("End of @Ignore");
 }
 
-//Capture and Compare(Ignored) Screenshot
+//Compare Base Screenshot
 @Test
 public void Test_Health_1_CompareScreenshot()throws Exception{
+System.out.println("Screenshot Compare");
 Health_check h = new Health_check();
 h.health_login(driver);
+
 CSVHandler login_details = new CSVHandler("src/test/resources/login_health_check_staging.csv");
 String baseurl0_name = login_details.getElementXpath("baseurl0_name");
 Date date = new Date();
 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 System.out.println(sdf.format(date));
 String time;
-String text;
 String str = sdf.format(date);
 System.out.println("STR:"+str);
 time = str.substring(0,2);
@@ -85,135 +81,19 @@ System.out.println("TIME:"+time);
 synchronized (driver) {driver.wait(15000);}
 WebElement ele = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/table/tbody/tr[2]/td[2]/div"));
 String result=null;
-String delivery_mode=null;
-CSVHandler general = null;
-general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
 
 //Make server CSV driven
 driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
-synchronized (driver){driver.wait(20000);}
+synchronized (driver){driver.wait(10000);}
 
-//Check the delivery mode
-String content_repository_delivery_mode = general.getElementXpath("content_repository_delivery_mode");
-text=driver.findElement(By.xpath(content_repository_delivery_mode)).getText();
-System.out.println(text);
-if(text.contains("Apache"))
-	delivery_mode="apache";
-else delivery_mode="svn";
-
-//Content Repository
-if(delivery_mode.equals("apache"))
-{
-	String content_repository_apache = general.getElementXpath("content_repository_apache");
-	text=driver.findElement(By.xpath(content_repository_apache)).getText();
-}
-else 
-{
-	String content_repository = general.getElementXpath("content_repository");
-	text=driver.findElement(By.xpath(content_repository)).getText();
-}
-	if(text.contains("Success"))
-	content_repository_check="true";
-
-text=null;
-
-//SRI1
-if(delivery_mode.equals("apache"))
-{
-	String sri1_apache = general.getElementXpath("sri1_apache");
-	text=driver.findElement(By.xpath(sri1_apache)).getText();
-}
-else 
-{
-	String sri1 = general.getElementXpath("sri1");
-	text=driver.findElement(By.xpath(sri1)).getText();
-}
-if(text.contains("Success"))
-	sri1_check="true";
-
-text=null;
-
-//SRI2
-if(delivery_mode.equals("apache"))
-{
-	String sri2_apache = general.getElementXpath("sri2_apache");
-	text=driver.findElement(By.xpath(sri2_apache)).getText();
-}
-else 
-{
-	String sri2 = general.getElementXpath("sri2");
-	text=driver.findElement(By.xpath(sri2)).getText();
-}
-if(text.contains("Success"))
-	sri2_check="true";
-
-text=null;
-
-//SRI3
-if(delivery_mode.equals("apache"))
-{
-	String sri3_apache = general.getElementXpath("sri3_apache");
-	text=driver.findElement(By.xpath(sri3_apache)).getText();
-}
-else 
-{
-	String sri3 = general.getElementXpath("sri3");
-	text=driver.findElement(By.xpath(sri3)).getText();
-}
-if(text.contains("Success"))
-	sri3_check="true";
-
-text=null;
-
-//Webassets
-String webassets = general.getElementXpath("webassets");
-text=driver.findElement(By.xpath(webassets)).getText();
-if(text.contains("Success"))
-	webassets_check="true";
-
-text=null;
-
-//Authorization API
-if(delivery_mode.equals("apache"))
-{
-	String auth_api_apache = general.getElementXpath("auth_api_apache");
-	text=driver.findElement(By.xpath(auth_api_apache)).getText();
-}
-else 
-{
-	String auth_api = general.getElementXpath("auth_api");
-	text=driver.findElement(By.xpath(auth_api)).getText();
-}
-if(text.contains("Success"))
-	auth_api_check="true";
-
-text=null;
-
-//Database
-String database = general.getElementXpath("database");
-text=driver.findElement(By.xpath(database)).getText();
-if(text.contains("Success"))
-	database_check="true";
-
-text=null;
-
-//Wowza
-if(delivery_mode.equals("apache"))
-{
-	String wowza_apache = general.getElementXpath("wowza_apache");
-	text=driver.findElement(By.xpath(wowza_apache)).getText();
-}
-else 
-{
-	String wowza = general.getElementXpath("wowza");
-	text=driver.findElement(By.xpath(wowza)).getText();
-}
-if(text.contains("Success"))
-	wowza_check="true";
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String wowza = general.getElementXpath("wowza");
+driver.findElement(By.xpath(wowza)).getText();
 
 Screenshot.takeElementScreenshot(driver, ele, ele.getLocation(), time + "_"+"actual_health_"+baseurl0_name);
 
-/* Compare Screenshot
+/*
 result = Screenshot.compareScreenshots("actual_health_"+baseurl0_name, "base_health_"+baseurl0_name);
 if(result.equals("True"))
 {
@@ -226,41 +106,188 @@ TestAssertion.assertionEquals(driver, "true", global_check);
 
 @Test
 public void TestHealth_2_ContentRepository()throws Exception{
-TestAssertion.assertionEquals(driver, "true", content_repository_check);
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("Check Content Repository");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String content_repository = general.getElementXpath("content_repository");
+text=driver.findElement(By.xpath(content_repository)).getText();
+System.out.println("Check Content Repository : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
 }
 
 @Test
 public void TestHealth_3_SRI1()throws Exception{
-TestAssertion.assertionEquals(driver, "true", sri1_check);
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("SRI 1");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String sri1 = general.getElementXpath("sri1");
+text=driver.findElement(By.xpath(sri1)).getText();
+System.out.println("SRI 1 : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
 }
 
 @Test
 public void TestHealth_4_SRI2()throws Exception{
-TestAssertion.assertionEquals(driver, "true", sri2_check);
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("SRI 2");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String sri2 = general.getElementXpath("sri2");
+text=driver.findElement(By.xpath(sri2)).getText();
+System.out.println("SRI 2 : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
 }
 
 @Test
 public void TestHealth_5_SRI3()throws Exception{
-TestAssertion.assertionEquals(driver, "true", sri3_check);
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("SRI 3");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String sri3 = general.getElementXpath("sri3");
+text=driver.findElement(By.xpath(sri3)).getText();
+System.out.println("SRI 3 : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
 }
 
 @Test
-public void TestHealth_6_Webassets()throws Exception{
-TestAssertion.assertionEquals(driver, "true", webassets_check);
+public void TestHealth_7_Wowza()throws Exception{
+System.out.println(global_check);
+String server;
+String text;
+String check = "false";
+CSVHandler general = null;
+if(!global_check.equals("true"))
+{	
+System.out.println("Wowza");
+Health_check h = new Health_check();
+h.health_login(driver);
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+server = general.getElementXpath("baseurl0_name");
+//synchronized (driver){driver.wait(10000);}
+String wowza = general.getElementXpath("wowza");
+text=driver.findElement(By.xpath(wowza)).getText();
+System.out.println("Wowza : "+text);
+if((!server.equals("staging")))
+	{
+	if(text.contains("Failed"))
+		check="true";
+	TestAssertion.assertionEquals(driver, "true", check);
+	}
+else if((server.equals("staging")))
+	{	
+	if(text.contains("Success"))
+		check="true";
+	TestAssertion.assertionEquals(driver, "true", check);
+	}
 }
-
-@Test 
-public void TestHealth_7_AuthorizationAPI()throws Exception{
-TestAssertion.assertionEquals(driver, "true", auth_api_check);
 }
 
 @Test
-public void TestHealth_8_Databse()throws Exception{
-TestAssertion.assertionEquals(driver, "true", database_check);
+public void TestHealth_8_AuthorizationAPI()throws Exception{
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("Authorization API");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String auth_api = general.getElementXpath("auth_api");
+text=driver.findElement(By.xpath(auth_api)).getText();
+System.out.println("Authorization API : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
 }
 
 @Test
-public void TestHealth_09_Wowza()throws Exception{
-TestAssertion.assertionEquals(driver, "true", wowza_check);
+public void TestHealth_9_Databse()throws Exception{
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("Database");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String database = general.getElementXpath("database");
+text=driver.findElement(By.xpath(database)).getText();
+System.out.println("Database : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
+}
+
+@Test
+public void TestHealth_10_Webassets()throws Exception{
+System.out.println(global_check);
+if(!global_check.equals("true"))
+{	
+System.out.println("Webassets");
+Health_check h = new Health_check();
+h.health_login(driver);
+CSVHandler general = null;
+general = new CSVHandler("src/test/resources/login_health_check_staging.csv");
+String text;
+String check = "false";
+//synchronized (driver){driver.wait(10000);}
+String webassets = general.getElementXpath("webassets");
+text=driver.findElement(By.xpath(webassets)).getText();
+System.out.println("Webassets : "+text);
+if(text.contains("Success"))
+	check="true";
+TestAssertion.assertionEquals(driver, "true", check);
+}
 }
 }
